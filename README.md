6c6542bd2eaf10b339d5531105c9cc69210b5a47
neerajlk <neerajkamat8@gmail.com>
2020-08-20 15:40:58 +0530

    added readme and removed unwanted code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# notes-api
-# notes-api
+# To-do Notes - API
+REST API using NodeJs, Express , MongoDB (CRUD operations)
+
+# Setup Guide
+Clone and take a pull from master branch and install node modules using `npm install`.
+create a collection named `Notes` in the mongodb server that is connected(default localhost).
+start the server with command `npm start` from root project directory.
+
+# Login and Register
+ Register - POST : `/user` , payload : { "name" : 'your-name' "email" :'your-email' , "password": 'your-password'}
+ Login - POST : `/login` , payload : { "email" :'your-email' , "password": 'your-password'}
+
+# To-Do-Notes
+ Create Note - POST : `/note` ,  Headers : {Authorization : 'Token-generated-from-login'}
+
+ Edit Note - PUT : `/note/:id` , (`:id`- Note ID), Headers : {Authorization : 'Token-generated-from-login'}
+
+ Delete Note - DELETE : `/note/:id` , (`:id`- Note ID) , Headers : {Authorization : 'Token-generated-from-login'}
+ 
+ Get Note - GET : `/note/:id` , (`:id`- Note ID) , Headers : {Authorization : 'Token-generated-from-login'}
+ 
+ Get All Notes - GET : `/notes` , (`:id`- Note ID) , params : { "page" :'page-no' , "limit": 'limit'} , Headers : {Authorization : 'Token-generated-from-login'}
--- app/controllers/registerUser.controller.js
+++ app/controllers/registerUser.controller.js
@@ -3,7 +3,7 @@ const Bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 
-// Create and Save a new Note
+// Create and Save a new User
 exports.create = (req, res) => {
     // Validate request
 
@@ -45,7 +45,7 @@ exports.create = (req, res) => {
 
 
 
-
+// User Login
 exports.login = (req, res) => {
     User.findOne({ "email": req.body.email }).then(user => {
         if (Bcrypt.compareSync(req.body.password, user.password)) {
@@ -87,7 +87,7 @@ exports.login = (req, res) => {
     });
 }
 
-// Find a single note with a noteId
+// Find a single User with a userId
 exports.findOne = (req, res) => {
     User.findById(req.params.userId, 'name email')
         .then(person => {
@@ -109,79 +109,3 @@ exports.findOne = (req, res) => {
         });
 };
 
-
-    // try {
-
-    //     const userCred = new User({
-    //         email: req.body.email,
-    //         password: Bcrypt.hashSync(req.body.password, 10)
-    //     });
-
-    //     var user = await UserModel.findOne({ username: request.body.username }).exec();
-    //     if(!user) {
-    //         return response.status(400).send({ message: "The username does not exist" });
-    //     }
-    //     if(!Bcrypt.compareSync(request.body.password, user.password)) {
-    //         return response.status(400).send({ message: "The password is invalid" });
-    //     }
-    //     response.send({ message: "The username and password combination is correct!" });
-    // } catch (error) {
-    //     response.status(500).send(error);
-    // }
-
-
-
-// Update a note identified by the noteId in the request
-// exports.update = (req, res) => {
-//     // Validate Request
-//     if (!req.body.content) {
-//         return res.status(400).send({
-//             message: "Note content can not be empty"
-//         });
-//     }
-
-//     // Find note and update it with the request body
-//     Note.findByIdAndUpdate(req.params.noteId, {
-//         title: req.body.title || "Untitled Note",
-//         content: req.body.content
-//     }, { new: true })
-//         .then(note => {
-//             if (!note) {
-//                 return res.status(404).send({
-//                     message: "Note not found with id " + req.params.noteId
-//                 });
-//             }
-//             res.send(note);
-//         }).catch(err => {
-//             if (err.kind === 'ObjectId') {
-//                 return res.status(404).send({
-//                     message: "Note not found with id " + req.params.noteId
-//                 });
-//             }
-//             return res.status(500).send({
-//                 message: "Error updating note with id " + req.params.noteId
-//             });
-//         });
-// };
-
-// Delete a note with the specified noteId in the request
-// exports.delete = (req, res) => {
-//     Note.findByIdAndRemove(req.params.noteId)
-//         .then(note => {
-//             if (!note) {
-//                 return res.status(404).send({
-//                     message: "Note not found with id " + req.params.noteId
-//                 });
-//             }
-//             res.send({ message: "Note deleted successfully!" });
-//         }).catch(err => {
-//             if (err.kind === 'ObjectId' || err.name === 'NotFound') {
-//                 return res.status(404).send({
-//                     message: "Note not found with id " + req.params.noteId
-//                 });
-//             }
-//             return res.status(500).send({
-//                 message: "Could not delete note with id " + req.params.noteId
-//             });
-//         });
-// };
